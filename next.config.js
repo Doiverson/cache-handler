0462d615aeabd52bb49d09458957038d20719eff
Doiverson <doiverson.canada@gmail.com>
2024-08-24 20:33:03 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  cacheHandler:
-    process.env.NODE_ENV === "production"
-      ? require.resolve("./cache-handler.js")
-      : undefined,
+  cacheHandler: require.resolve('./cache-handler.js'),
   env: {
-    NEXT_PUBLIC_REDIS_INSIGHT_URL:
-      process.env.REDIS_INSIGHT_URL ?? "http://localhost:8001",
+    NEXT_PUBLIC_REDIS_INSIGHT_URL: process.env.REDIS_INSIGHT_URL ?? 'http://localhost:8001',
   },
 };
 
